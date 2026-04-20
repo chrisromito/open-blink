cb7c79903c1e0a0751b3068a7f1716607cd48656
Chris Romito <chris@modii.co>
2026-04-20 14:25:34 -0600

    feature/detection-service-unix-ports - Cleanup and add scripts for initial install & systemd services

---
+++ detection_service/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+sudo apt install -y python3-picamera2 --no-install-recommends
+sudo apt install -y iptables
+cd $HOME/open-blink/detection_service
+echo "Setting up venv..."
+python3 -m venv venv
+source ./venv/bin/activate
+pip install -r requirements.txt
+echo "Setting permissions on run.sh..."
+chmod +x ./run.sh
+echo "Setting up systemd service"
+sudo cp ./systemd/open-blink-detection.service /etc/systemd/system/open-blink-detection.service
+echo "Reloading systemctl..."
+sudo systemctl daemon-reload
+# Enable services on boot
+echo "Enabling services"
+sudo systemctl enable open-blink-detection.service
+echo "Starting open-blink-detection.service..."
+sudo systemctl start open-blink-detection.service
+echo "    Success"
+echo "Installation complete"
---
+++ devicecapture/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+cd $HOME/open-blink
+echo "Setting permissions on run.sh..."
+chmod +x ./run.sh
+echo "Setting up systemd service"
+cd ./devicecapture
+sudo cp ./systemd/open-blink-server.service /etc/systemd/system/open-blink-server.service
+echo "Reloading systemctl..."
+sudo systemctl daemon-reload
+# Enable services on boot
+echo "Enabling services"
+sudo systemctl enable open-blink-server.service
+echo "Starting open-blink-server.service..."
+sudo systemctl start open-blink-server.service
+echo "    Success"
+echo "Installation complete"
